f0868881b61e064872f843810e9f900a48d16c93
rover1 <evard123@gmail.com>
2021-04-30 08:25:27 -0600

    Initial push

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rover1/rover_repo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rover1/rover_repo/devel;/home/rover1/catkin_rover/devel;/opt/ros/melodic'.split(';') if '/home/rover1/rover_repo/devel;/home/rover1/catkin_rover/devel;/opt/ros/melodic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/rover1/catkin_rover/devel/lib:/opt/ros/melodic/lib'
+export PWD='/home/rover1/rover_repo/build'
